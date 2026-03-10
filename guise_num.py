b6f9689a3ff6b997e775da84b3dd69d527524f64
mdnaim2004 <naim.cse2004@gmail.com>
2026-03-10 10:30:50 +0600

    number gusing tool

---
+++ guise_num.py
@@ -0,0 +1,21 @@
+import time
+print("AI mind Rider")
+time.sleep(1)
+
+print("Thinking of a number between 1 to 100")
+input("press Enter when you're ready...")
+
+low = 1
+high = 100
+while low<= high:
+    mid = (low+high) // 2
+    answer = input(f"\nIs your number greater than {mid}?(y/n): ")
+    
+    if answer.lower() == "y":
+        low = mid + 1
+    else:
+        high = mid - 1
+
+print("\nAI Predection complete")
+print("Your number is : ", low)
+
